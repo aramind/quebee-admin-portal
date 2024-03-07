fd9ce6203230a776fa512d1ff6c1bb6025cdf22a
aramind <88814622+aramind@users.noreply.github.com>
2024-03-07 13:56:27 +0800

    feat: fixing the responsiveness of add user section

--- src/containers/FormActionsContainer.jsx
+++ src/containers/FormActionsContainer.jsx
@@ -3,7 +3,11 @@ import React from "react";
 
 const FormActionsContainer = ({ children }) => {
   return (
-    <Stack direction="row" gap={2} sx={{ justifyContent: "flex-end", py: 0 }}>
+    <Stack
+      direction={{ xs: "column", sm: "row" }}
+      gap={2}
+      sx={{ justifyContent: { sm: "flex-end", xs: "center" }, py: 0 }}
+    >
       {children}
     </Stack>
   );
