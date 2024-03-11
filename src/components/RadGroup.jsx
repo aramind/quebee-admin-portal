d3dd581291a209492822dcbebf137a30eebe7772
aramind <88814622+aramind@users.noreply.github.com>
2024-03-11 11:56:43 +0800

    feat: making the selected on radiogroups be bold and colored when selected

--- src/components/RadGroup.jsx
+++ src/components/RadGroup.jsx
@@ -1,22 +1,40 @@
-import { FormControlLabel, Radio, RadioGroup } from "@mui/material";
+import { FormControlLabel, Radio, RadioGroup, Typography } from "@mui/material";
+import { blue, red } from "@mui/material/colors";
 import React from "react";
-import constants from "./configs/constants";
-import { red } from "@mui/material/colors";
 
 const RadGroup = ({ field, options, row }) => {
+  console.log("field.value:", field.value);
   return (
     <RadioGroup
       aria-labelledby="radio-group"
       value={field.value}
       row={row}
       onChange={(e) => field.onChange(e.target.value)}
+      sx={{ pt: 0.5, pb: 0, px: 2 }}
     >
       {options.map((option) => (
         <FormControlLabel
           key={typeof option === "object" ? option.key : option}
           value={typeof option === "object" ? option.key : option}
           control={<Radio size="small" />}
-          label={typeof option === "object" ? option.value : option}
+          // label={typeof option === "object" ? option.value : option}
+          label={
+            <Typography
+              sx={{
+                fontSize: "0.9rem",
+                color:
+                  (typeof option === "object"
+                    ? field.value === option.key
+                    : field.value === option) && "primary.dark",
+                fontWeight:
+                  (typeof option === "object"
+                    ? field.value === option.key
+                    : field.value === option) && "bold",
+              }}
+            >
+              {typeof option === "object" ? option.value : option}
+            </Typography>
+          }
         />
       ))}
     </RadioGroup>
