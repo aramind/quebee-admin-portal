8b91055efa97337ac9ad97ce63941d825c48716f
aramind <88814622+aramind@users.noreply.github.com>
2024-03-23 23:31:11 +0800

    feat: adding users to the db via add new user section

--- src/hooks/useAddUser.js
+++ src/hooks/useAddUser.js
@@ -1,9 +1,10 @@
 import { useMutation } from "react-query";
 import axios from "axios";
 
-const API_URL = `${process.env.REACT_APP_API_URL}users`;
+const API_URL = `${process.env.REACT_APP_API_URL}/users`;
 
 const addUser = (user) => {
+  console.log(API_URL);
   return axios.post(API_URL, user);
 };
 
