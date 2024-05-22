c28ddef3933fc4ea3ecd23628de1655c382c5540
aramind <88814622+aramind@users.noreply.github.com>
2024-05-23 02:17:19 +0800

    feat: making the manage question page fetch the list of question from db

--- src/pages/question/ManageQuestionTab.jsx
+++ src/pages/question/ManageQuestionTab.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 import ManageQuestionPage from "../manage-question-page/ManageQuestionPage";
+import useQuestionReq from "../../hooks/api/useQuestionReq";
+import useApiGet from "../../hooks/api/useApiGet";
 
 const ManageQuestionTab = () => {
+  const { get } = useQuestionReq();
+
+  const { data: questionsList } = useApiGet(
+    "questions",
+    () => get("/trimmed"),
+    {
+      retry: 3,
+    }
+  );
+
+  console.log(questionsList);
   return (
     <div>
-      <ManageQuestionPage />
+      {questionsList &&
+        questionsList.map((q, i) => {
+          return (
+            <p>
+              {i + 1}) {q.question?.text}
+              <ul>
+                {q.choices?.map((c) => (
+                  <li>
+                    {c.value.text} {c.isCorrect ? "*" : ""}
+                  </li>
+                ))}
+              </ul>
+              <br />
+            </p>
+          );
+        })}
     </div>
   );
 };
